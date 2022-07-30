91ae3f5fda4ede757832710e74f858b2ceb12207
mcquintao <mcquintao.qui@gmail.com>
2022-07-29 22:45:35 -0300

    Adicao de itens

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,55 @@
 import 'dart:collection';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:uuid/uuid.dart';
+
 void main() {
-  runApp(MaterialApp(
+  runApp(ChangeNotifierProvider(
+    create: (context) => ShopListProvider(),
+    child: MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: const HomePage(),
-    ));
+      routes: {
+        '/new' :(context) => const NewItemPage(),
+      },
+    ),
+  ));
 }
 
-
-enum Unity {
-  uni,
-  kg,
-  g,
-  L,
-  mL
-}
+enum Unity { uni, kg, g, L, mL }
 
 class Item {
   final String name;
-  final String qtdd;
-  final Unity unity;
   final String id;
   bool isChecked;
 
-  Item({required this.name, required this.qtdd, required this.unity}) : id = const Uuid().v4(), isChecked = false;
+  Item({required this.name})
+      : id = const Uuid().v4(),
+        isChecked = false;
 
-  @override bool operator ==(covariant Item other) => id == other.id;
-  
-  @override int get hashCode => id.hashCode;
-  
+  @override
+  bool operator ==(covariant Item other) => id == other.id;
+
+  @override
+  int get hashCode => id.hashCode;
+
   void changeCheck() {
     isChecked = !isChecked;
   }
 
   @override
   String toString() {
-    return "$qtdd $unity de $name";
+    return name;
   }
-
 }
 
-class ShopList extends ChangeNotifier{
+class ShopListProvider extends ChangeNotifier {
   final List<Item> _itemList = [];
   UnmodifiableListView<Item> get list => UnmodifiableListView(_itemList);
-
 
   void addItem(Item item) {
     _itemList.add(item);
@@ -64,7 +65,18 @@ class ShopList extends ChangeNotifier{
     _itemList.clear();
     notifyListeners();
   }
+}
 
+class ShopListWidget extends StatelessWidget {
+  final UnmodifiableListView<Item> shopList;
+  const ShopListWidget({super.key, required this.shopList});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(children: shopList.map((item) => Card(child: Text(item.name),)).toList(),),
+    );
+  }
 }
 
 class HomePage extends StatelessWidget {
@@ -74,8 +86,66 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page'),
+        title: const Text('Lista de compras'),
       ),
+      body: SafeArea(
+          child: Column(
+        children: [
+          Consumer<ShopListProvider>(builder: (context, value, child) {
+            return ShopListWidget(shopList: value.list,);
+          },)
+        ],
+      )),
+      floatingActionButton:
+          FloatingActionButton(onPressed: () {
+            Navigator.pushNamed(context, '/new');
+          }, child: const Icon(Icons.add)),
+    );
+  }
+}
+
+class NewItemPage extends StatefulWidget {
+  const NewItemPage({super.key});
+
+  @override
+  State<NewItemPage> createState() => _NewItemPageState();
+}
+
+class _NewItemPageState extends State<NewItemPage> {
+  late final TextEditingController _controllerName;
+
+
+  @override
+  void initState() {
+    _controllerName = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controllerName.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Novo item")),
+      body: Column(children: [
+        TextField(
+          controller: _controllerName,
+          decoration: const InputDecoration(hintText: "Nomde do item"),
+        ),
+        ElevatedButton(onPressed: () {
+          if(_controllerName.value.text.isNotEmpty) {
+            final Item item = Item(name: _controllerName.value.text);
+          context.read<ShopListProvider>().addItem(item);
+          print("clicou");
+          Navigator.of(context).pop();
+          }
+        }, child: const Text("Adicionar"))
+      ]),
     );
   }
 }
